d42b3e9f26c78c512997e14e19e2a7e77f08baf1
Mohammed Emad <mohammedmakhlouf78@gmail.com>
2020-11-06 18:42:59 +0200

    end

--- script.js
+++ script.js
@@ -2,14 +2,34 @@ let dec = document.querySelector('#dec')
 let count = document.querySelector('#count')
 dec.addEventListener('click',(e) => {
     count.textContent -=1
+    countColor(count.textContent)
 })
 
 let reset = document.querySelector('#reset')
 reset.addEventListener('click',(e) => {
     count.textContent =0
+    countColor(count.textContent)
 })
 
 let inc = document.querySelector('#inc')
 inc.addEventListener('click', (e) => {
     count.textContent = parseInt(count.textContent)+1
+    countColor(count.textContent)
 })
+
+
+function countColor(num)
+{
+    if(num>0)
+    {
+        count.style.color = 'green'
+    }
+    else if(num < 0)
+    {
+        count.style.color = 'red'
+    }
+    else
+    {
+        count.style.color = 'black'
+    }
+}
